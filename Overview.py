19d23a32531357ade16beccb553e51f9cd23321b
Khh98 <73216228+Khh98@users.noreply.github.com>
2023-03-13 23:29:41 +0200

    Update Overview.py

--- Overview.py
+++ Overview.py
@@ -28,7 +28,6 @@ html_code= """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello</title>
     <style>
         body {
             margin: 0;
@@ -42,41 +41,10 @@ html_code= """
             background-position: center;
             background-size: cover;
         }
-        #content {
-            height: 100vh;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            color: black;
-            font-family: Verdana, sans-serif;
-        }
-        h1 {
-            font-size: 2rem;
-            margin: 0;
-        }
-        .text-block {
-            text-align: center;
-            margin-top: 5px;
-        }
-        button {
-            background-color: white;
-            color: #222;
-            font-weight: bold;
-            border: 2px solid #222;
-            padding: 10px 20px;
-            border-radius: 5px;
-        }
     </style>
 </head>
 <body>
     <div id="background"></div>
-    <div id="content">
-        <h1>Hello, I'm</h1>
-        <h1>Karim Hazimeh</h1>
-        <div class="text-block">a Data detective with a Masters in Business Analytics🕵🏻‍♀️.<br>I am passionate about utilizing my tech-savviness and business analytics expertise to uncover insights from data that guide end-users through the vast and ever-changing <b>sky of possibilities.</b></div>
-        <br><a href="https://drive.google.com/uc?export=download&id=1__wsAULlJ3uoAYil0B77UODwsgWeEF5x" download><button>Download CV</button></a></br>
-    </div>
     <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.4.1.min.220afd743d.js" type="text/javascript" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://assets.website-files.com/5d971c4989c88c0b4cca55cd/js/webflow.1bc56dc86.js" type="text/javascript"></script>
     <script src="https://assets.website-files.com/5d932bf11608325eac058a21/5d932d68160832c7c0059c91_three.r92.min.txt"></script>
